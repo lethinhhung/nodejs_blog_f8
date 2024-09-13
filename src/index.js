53061de63e653acd96efed380fdbc9c65c8b9b04
Kong Trua <thung260803@gmail.com>
2024-09-13 20:37:07 +0700

    Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3001
 
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
 app.use(morgan('combined'))
 
 //Template engine
-app.engine('handlebars', handlebars.engine());
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views','./src/resources/views');
